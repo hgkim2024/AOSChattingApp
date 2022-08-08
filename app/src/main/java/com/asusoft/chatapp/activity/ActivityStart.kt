cc4e07c9a8fc9ca19b1e83b6cf4f43369230ae13
DESKTOP-T1VCKU6\gusrn <asukim2020@gmail.com>
2022-08-08 14:39:00 +0900

    gradle 필요 라이브러리 추가

--- app/src/main/java/com/asusoft/chatapp/activity/ActivityStart.kt
+++ app/src/main/java/com/asusoft/chatapp/activity/ActivityStart.kt
@@ -1,11 +1,12 @@
-package com.asusoft.chatapp
+package com.asusoft.chatapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asusoft.chatapp.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
     }
 }
